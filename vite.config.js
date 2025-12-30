9f8f8458bb55b0119344c62dfbaa3800d3462e07
Shanawaz Sharif <shanawaz@shanawazs-MacBook-Air.local>
2025-12-31 00:17:03 +0300

    fled vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/portfolio-website/",  // <--- ADD THIS LINE
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
